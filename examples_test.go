be1e4b8af260b33696ea599641ac409a66126f19
Edward Muller <me@freeformz.me>
2025-03-08 10:27:37 -0800

    add a basic concurrency test, separate examples

---
+++ examples_test.go
@@ -0,0 +1,57 @@
+package set
+
+import (
+	"fmt"
+	"slices"
+)
+
+func ExampleMap_Iterator() {
+	ints := New[int]()
+	ints.Add(5)
+	ints.Add(3)
+	ints.Add(2)
+	ints.Add(4)
+	ints.Add(1)
+
+	out := make([]int, 0, ints.Cardinality())
+	for i := range ints.Iterator {
+		out = append(out, i)
+	}
+
+	// sort the values for consistent output
+	slices.Sort(out)
+	for _, i := range out {
+		fmt.Println(i)
+	}
+	// Output:
+	// 1
+	// 2
+	// 3
+	// 4
+	// 5
+}
+
+func ExampleOrdered() {
+	ints := NewOrdered[int]()
+	ints.Add(5)
+	ints.Add(3)
+
+	AppendSeq(ints, slices.Values([]int{2, 4, 1}))
+	AppendSeq(ints, slices.Values([]int{5, 6, 1}))
+
+	out := make([]int, 0, ints.Cardinality())
+	for i := range ints.Iterator {
+		out = append(out, i)
+	}
+
+	for _, i := range out {
+		fmt.Println(i)
+	}
+	// Output:
+	// 5
+	// 3
+	// 2
+	// 4
+	// 1
+	// 6
+}
